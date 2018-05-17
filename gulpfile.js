e9b0ba7e12d8c29f154e66b6baaeebf54aa99a22
Arshpreet Kaur <arshpreetkaur989@gmail.com>
2018-05-16 20:33:16 -0500

    Concatenate all js files into 'main.js'

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ const autoprefixer = require('gulp-autoprefixer');
 let cleanCSS = require('gulp-clean-css');
 const imagemin = require('gulp-imagemin');
 const babel = require('gulp-babel');
+var concat = require('gulp-concat');
 
 gulp.task('css', () =>
 gulp.src('./src/css/**/*.css')
@@ -22,9 +23,14 @@ gulp.task('img', () =>
 
  
 gulp.task('js', () =>
-    gulp.src('./src/js/**/*.js')
+    gulp.src(['./src/js/resources.js', './src/js/app.js', './src/js/engine.js'])
         .pipe(babel({
             presets: ['env']
         }))
-        .pipe(gulp.dest('./dist/js'))
+        .pipe(concat('main.js'))
+        .pipe(gulp.dest('./dist/js/'))
 );
+
+ 
+
+
