233a0718409f2d5acdfc7a6cfdd54b09d9c1bd66
Arshpreet Kaur <arshpreetkaur989@gmail.com>
2018-05-16 19:54:57 -0500

    Minify the css file

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,13 @@
 const gulp = require('gulp');
 const autoprefixer = require('gulp-autoprefixer');
-
+let cleanCSS = require('gulp-clean-css');
+ 
 gulp.task('css', () =>
 gulp.src('./src/css/**/*.css')
     .pipe(autoprefixer({
         browsers: ['last 10 versions'],
         cascade: false
     }))
+    .pipe(cleanCSS({compatibility: 'ie8'}))
     .pipe(gulp.dest('./dist/css'))
 );
